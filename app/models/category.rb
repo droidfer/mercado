3c9f66aecfd086893aad78e3c4f4ae8b9669fe76
Fernando <fmartinez@dotech.com.py>
2023-05-23 14:53:46 -0400

    Index categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
   has_many :subcategories
-  
   validates :name, presence: true
+  
+  def show_name
+    return self.name if self.description.blank?
+    return "#{self.name} --D: #{self.description}"
+  end
 end
